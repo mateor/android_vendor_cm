54229543998d8b7df3d8000c6a19f1f5ff7d7dd0
mateor <mateorod9@gmail.com>
2012-09-24 19:12:13 -0500

    Add FileManager binary until I decide how I am going to handle the source code.

--- config/common.mk
+++ config/common.mk
@@ -51,7 +51,7 @@ PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/bin/handle_compcache:system/bin/handle_compcache
 
 PRODUCT_COPY_FILES +=  \
-#    vendor/cm/proprietary/RomManager.apk:system/app/RomManager.apk \
+    vendor/cm/proprietary/FileManager.apk:system/app/FileManager.apk \
 #    vendor/cm/proprietary/Term.apk:system/app/Term.apk \
 #    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so \
 	vendor/cm/prebuilt/common/apps/Superuser.apk:system/app/Superuser.apk
