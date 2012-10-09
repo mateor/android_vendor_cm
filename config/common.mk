5098708af0745124b7cc039a06773dd9267a93c5
mateor <mateorod9@gmail.com>
2012-10-08 22:56:54 -0500

    Include ParanoidPreferences.apk in personal builds

--- config/common.mk
+++ config/common.mk
@@ -96,6 +96,7 @@ PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/bin/modelid_cfg.sh:system/bin/modelid_cfg.sh
 
 PRODUCT_COPY_FILES +=  \
+     vendor/pa/proprietary/paranoid/ParanoidPreferences.apk
 #    vendor/cm/proprietary/Term.apk:system/app/Term.apk \
 #    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
 
